ca1980714cc6c86e2d156ad71bf162db63fe6a47
Donald Merand <dmerand@explo.org>
2018-04-10 11:04:57 -0400

    convert to regexp match instead of string equality for comparison. much more robust.

--- lib/show_and_tell/condition_generator.rb
+++ lib/show_and_tell/condition_generator.rb
@@ -5,33 +5,37 @@ module ShowAndTell
       @name = name
     end
 
-    def show_and_tell_when_equals(match_field, **fields_to_show)
-      show_when_equals match_field, fields_to_show
-      tell_when_equals match_field, fields_to_show
+    def show_and_tell_when_matches(matchable, **fields_to_show)
+      show_when_matches matchable, fields_to_show
+      tell_when_matches matchable, fields_to_show
     end
 
-    def show_when_equals(match_field, **fields_to_show)
+    def show_when_matches(matchable, **fields_to_show)
       show_map = @base.class_eval('@_show_map')
       show_map[@name] ||= {}
-      show_map[@name][match_field] = fields_to_show
+      show_map[@name][matchable] = fields_to_show
     end
 
-    def tell_when_equals(match_field, **fields_to_show)
-      validate_presence(match_field, fields_to_show) 
+    def tell_when_matches(matchable, **fields_to_show)
+      validate_presence(matchable, fields_to_show) 
     end
 
     private
 
-      def base_class_validate(field, msg, match_field)
+      def base_class_validate(field, msg, matchable)
         base_field_name = @name
-        @base.class_eval do 
-          validates_presence_of field, message: msg,
-            if: proc { |a| a.send(base_field_name) == match_field }
+        validation_logic = proc { |instance| 
+            field_value = instance.send(base_field_name)
+            field_value.nil? ? false : matchable.match(field_value.to_s)
+          }
+
+        @base.class_eval do |b| 
+          validates_presence_of field, message: msg, if: validation_logic
         end
       end
 
-      def validate_presence(match_field, fields_to_show)
-        fields_to_show.each { |f, msg| base_class_validate(f, msg, match_field) }
+      def validate_presence(matchable, fields_to_show)
+        fields_to_show.each { |f, msg| base_class_validate(f, msg, matchable) }
       end
   end
 end
--- test/show_and_tell_test.rb
+++ test/show_and_tell_test.rb
@@ -9,18 +9,18 @@ class FineCheese
   validates_presence_of :cheese
 
   form_option :cheese do |f| 
-    f.show_when_equals 'brie',
+    f.show_when_matches 'brie',
       age: 'How old is your Brie?'
 
-    f.tell_when_equals 'cheddar',
+    f.tell_when_matches /cheddar/i,
       origin: 'Kindly indicate the cheddar country'
 
-    f.show_and_tell_when_equals 'nacho',
+    f.show_and_tell_when_matches 'nacho',
       origin: 'Country plz on yer nacho chz'
   end
 
   form_option :age do |f|
-    f.tell_when_equals 42, moldiness: 'oooh, how moldy is it?'
+    f.tell_when_matches /42/, moldiness: 'oooh, how moldy is it?'
   end
 end
 
@@ -56,15 +56,25 @@ class ShowAndTellTest < Minitest::Test
     brie = FineCheese.new cheese: nil, age: 42
     refute brie.valid?
     assert_includes brie.errors.messages, :cheese
-    refute_includes brie.errors.messages, :origin
     assert_includes brie.errors.messages, :moldiness
     assert_equal 'oooh, how moldy is it?', brie.errors.messages[:moldiness].first
+    refute_includes brie.errors.messages, :origin
 
     cheddar = FineCheese.new cheese: 'cheddar'
     refute cheddar.valid?
-    refute_includes cheddar.errors.messages, :age
     assert_includes cheddar.errors.messages, :origin
     assert_equal 'Kindly indicate the cheddar country',
       cheddar.errors.messages[:origin].first
+    refute_includes cheddar.errors.messages, :age
+  end
+
+  def test_regexp_validation
+    cheddar_lc = FineCheese.new(cheese: 'cheddar')
+    refute cheddar_lc.valid?
+    assert_includes cheddar_lc.errors.messages, :origin
+
+    cheddar_uc = FineCheese.new(cheese: 'ChEdDaR')
+    refute cheddar_uc.valid?
+    assert_includes cheddar_uc.errors.messages, :origin
   end
 end
