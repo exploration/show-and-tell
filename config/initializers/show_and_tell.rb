44db493aa31248e3e0c2487d05935a411071cea5
Donald Merand <dmerand@explo.org>
2018-04-19 10:37:08 -0400

    update view initializer

--- config/initializers/show_and_tell.rb
+++ config/initializers/show_and_tell.rb
@@ -1,2 +1,2 @@
 # We want to add the ShowAndTell view helper by default
-ActiveSupport.on_load(:action_view_base) { include ShowAndTellRegisterHelper }
+ActionView::Base.send(:include, ShowAndTellRegisterHelper)
