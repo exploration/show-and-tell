99599f54ba8f21e64d40ad6e6a4394500d0add0b
Donald Merand <dmerand@explo.org>
2018-04-19 12:43:21 -0400

    re-raw register helper

--- lib/show_and_tell/register_helper.rb
+++ lib/show_and_tell/register_helper.rb
@@ -1,9 +1,10 @@
 module ShowAndTellRegisterHelper
   def show_and_tell_register(klass)
-    <<~TEMPLATE
+    template = <<~TEMPLATE
       <script>
         show_and_tell.register("#{klass}", #{klass.show_and_tell_json})
       </script>
     TEMPLATE
+    defined?(Rails) ? raw(template) : template
   end 
 end
