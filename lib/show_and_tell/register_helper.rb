47a32da44f260aef39ccfa448b4d18397f308b31
Donald Merand <dmerand@explo.org>
2018-04-19 10:44:02 -0400

    use raw if rails

--- lib/show_and_tell/register_helper.rb
+++ lib/show_and_tell/register_helper.rb
@@ -1,9 +1,10 @@
 module ShowAndTellRegisterHelper
   def show_and_tell_register(klass)
-    <<~JAVASCRIPT
+    template = <<~TEMPLATE
       <script>
         show_and_tell.register("#{klass}", #{klass.show_and_tell_json})
       </script>
-    JAVASCRIPT
+    TEMPLATE
+    defined?(Rails) ? raw(template) : template
   end 
 end
